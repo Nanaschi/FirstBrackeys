910c0c49c8bf747e3b61fbcd20ba05defc95170e
Daniel <Daniel@DESKTOP-5U9J4R6>
2024-05-16 12:50:33 +0700

    Implemented running

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -18,9 +18,18 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("move_left", "move_right")
+	
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("default")
+		else:
+			animated_sprite.play("run")
+	else :
+		animated_sprite.play("jump")
 	
 	if direction > 0:
 		animated_sprite.flip_h = false
